924b002fe1a02fb3c40daddecb37c47f738ad914
Jan Hartmann <95341131+JamoHartmann@users.noreply.github.com>
2022-07-26 12:22:31 +0200

    Update Programm.bat

--- Example/Programm.bat
+++ Example/Programm.bat
@@ -2,4 +2,6 @@ start "Quality" call "x64\Release\PointCloud__Quality.exe" ^
 		-P "H:\Daten_Hitec\ZF_Projekt\Export\3.e57"^
 		-S "Scanner ++.txt"^
 		-VKV "VCV_3.txt"^
+		-Edge 100^
+		-IA 100^
 		-R "Result.ply"
